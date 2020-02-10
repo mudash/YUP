0d0a765810f59cf2f145a3cf607825297f25553c
Mudassar Shafique <15931574+mudash@users.noreply.github.com>
2020-02-10 06:48:38 +0500

    Adding habitat plan, removing archive folder

---
+++ habitat/plan.sh
@@ -0,0 +1,9 @@
+pkg_name=yup
+pkg_origin=chefshafique
+pkg_version="0.1.0"
+pkg_scaffolding="core/scaffolding-go"
+pkg_maintainer="The Chef Effortless Team <sales@chef.io>"
+pkg_license=('Proprietary, All rights reserved')
+pkg_bin_dirs=(bin)
+pkg_source="https://github.com/mudash/YUP"
+pkg_build_deps=(core/gcc core/go core/git core/make)
